f7d1d83b927eaaeb470e39bc211c4b3afa71817e
JAYVER <ujayrosh.akg@gmail.com>
2018-10-25 18:25:16 +0530

    Commiting Changes for folder read.

--- filehandler/automate_process.js
+++ filehandler/automate_process.js
@@ -1,28 +1,71 @@
 var fs = require('fs')
 const testFolder = './inbox';
 var loadcsv = require('./test.js');
-var async = require('async')
+var async = require('async');
+var createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 exports.automate_data_load =function (req,res) {
-    fs.readdirSync(testFolder).forEach(file => {
-        console.log("parent:-"+file);
-    var items = fs.readdirSync(testFolder+"/"+file);
-    async.each(items, function(item, callback){
-            console.log("child:-"+item);
-            req.params.object_id = "Collection";
-            req.params.file_location = testFolder+"/"+file+"/"+item;
-            loadcsv.upload_document(req,res);
-            callback();
-        },
-        function(err){
-            console.log("All Completes :");
+    const csvWriter = createCsvWriter({
+        path: 'file.csv',
+        header: [
+            {id: 'file_location', title: 'File Location'},
+            {id: 'logs', title: 'Logs'}
+        ]
+    });
+    var records =[];
+    var parentFolder = fs.readdirSync(testFolder);
+    async.each(parentFolder, function(parentItem, callback){
+        console.log("parent:-"+parentItem);
+        var items = fs.readdirSync(testFolder+"/"+parentItem);
+        async.each(items, function(item, callback){
+                req.params.object_id = "Collection";
+                req.params.file_location = testFolder+"/"+parentItem+"/"+item;
+                var file_location = testFolder+"/"+parentItem+"/"+item;
+                var csv_arr =[];
+                async.series([
+                        function (callback) {
+                            console.log("Getting CSV content of :--"+file_location)
+                            loadcsv.get_csv_data(file_location, csv_arr, callback)
+                        },
+                        function (callback) {
+                            console.log("Starting uploading CSV --"+csv_arr)
+                            loadcsv.upload_document(req,res,csv_arr, records, callback);
+                        }
+                    ],
+                    function (err) {
+                        if (err) {
+                            callback(err);
+                            /*res.status(400).send({
+                                "error": err
+                            });*/
+                        } else {
+                            callback();
+                            /*res.status(200).send({
+                                "document_id": "OK"
+                            });*/
+                        }
+                    });
+            },
+            function(err){
+                console.log("All Completes :"+items);
+                callback();
+            }
+        );
+    },
+    function(err){
+        console.log("Final Complete :");
+        csvWriter.writeRecords(records)       // returns a promise
+            .then(() => {
+            console.log('Finaly written and Done');
+        });
+        if (err) {
+            res.status(400).send({
+                "error": err
+            });
+        } else {
+            res.status(200).send({
+                "document_id": "OK"
+            });
         }
-    );
-        /*fs.readdirSync(testFolder+"/"+file).forEach(file_child => {
-            console.log("child:-"+file_child);
-            req.params.object_id = "Collection";
-            req.params.file_location = testFolder+"/"+file+"/"+file_child;
-            loadcsv.upload_document(req,res);
-        })*/
-    })
+    });
 }
--- filehandler/test.js
+++ filehandler/test.js
@@ -12,39 +12,48 @@ var end_time;
 var fs = require("fs");
 var createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
-exports.upload_document = function (req, res) {
+exports.upload_document = function (req, res, csv_arr,records, callback) {
 
     var object_id = req.params.object_id;
-    var file_location = req.params.file_location;
 
     console.log("Start The Loading at : " + start_time);
     async.series([
             function (callback) {
-                this_file.get_csv_data(file_location,callback);
+                //this_file.get_csv_data(file_location,callback);
+                callback();
             },
             function (callback) {
                 async.each(csv_arr, function(csv_arr_val, callback1){
-                        this_file.callDatabase(csv_arr_val, object_id, callback1);
-                    }, function(err) {
-                        if( err ) {
-                            console.log('A file failed to process');
-                        } else {
-                            console.log('All files have been processed successfully');
-                            callback();
-                        }
+                    var record ={};
+                    record.file_location = req.params.file_location;
+                    this_file.callDatabase(csv_arr_val, object_id, function (body) {
+                        record.logs = body;
+                        records.push(record);
+                        callback1();
+                    });
+                }, function(err) {
+                    if( err ) {
+                        console.log('A file failed to process');
+                        callback(err);
+                    } else {
+                        console.log('All files have been processed successfully');
+                        callback();
                     }
+                }
                 );
             }
         ],
         function (err) {
             if (err) {
-                res.status(400).send({
+                callback(err);
+                /*res.status(400).send({
                     "error": err
-                });
+                });*/
             } else {
-                res.status(200).send({
+                callback();
+                /*res.status(200).send({
                     "document_id": "OK"
-                });
+                });*/
             }
         });
 }
@@ -54,19 +63,20 @@ exports.callDatabase = function (val, object_id, callback) {
     var options = {
 
         method: 'POST',
-        url: 'https://ngpsservice--tst2.custhelp.com/cc/webservice/callfm/' + object_id + 'Collection/' + val,
+        url: 'https://ngpsservice--tst2.custhelp.com/cc/webservice/callfm/' + object_id + '/' + val,
         pool: separateReqPool
     };
 
     request(options, function (error, response, body) {
+        console.log(error)
         if (error) throw new Error(error);
         console.log(body);
-        callback();
+        callback(body);
     });
 }
 
-exports.get_csv_data = function (file_location, callback) {
-    var csvstream = csv.fromPath('./' + file_location, {headers: true})
+exports.get_csv_data = function (file_location, csv_arr,callback) {
+    var csvstream = csv.fromPath(file_location, {headers: true})
         .on("data", function (row) {
             csv_arr.push(Object.values(row));
         })
